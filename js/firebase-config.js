977bbf2d61c7b5651557df166546568368e969e5
GAYOSM <146345000+GAYOSM@users.noreply.github.com>
2026-02-20 07:23:23 +0530

    Firebase update connection

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,24 +1,18 @@
-// js/firebase-config.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+// Firebase Config (Compat version)
 
 const firebaseConfig = {
   apiKey: "AIzaSyDX5tR2JF6wZYoKfxmZY4fMMUh1YZSponQ",
   authDomain: "mizpahfoundations-70c90.firebaseapp.com",
   projectId: "mizpahfoundations-70c90",
-  storageBucket: "mizpahfoundations-70c90.firebasestorage.app",
+  storageBucket: "mizpahfoundations-70c90.appspot.com",
   messagingSenderId: "1066452149935",
   appId: "1:1066452149935:web:73d0b2e0370066d78e565a"
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Make globally available
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
